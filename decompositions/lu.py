08dbb296534e72e2f39c2c5107df29e876b6c4e4
Tommy Nguyen <haidangasian@gmail.com>
2025-07-12 15:57:00 +0700

    refactored code for LU and QR decomp

---
+++ decompositions/lu.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def decompose_lu(A): # partial pivoting
+    A = A.copy.astype(float)
+    n = len(A)
+    perm = np.arange(n)
+
+    for k in range(n - 1):
+
+        # find pivot row
+        p = np.argmax(np.abs(A[k:, k])) + k
+        if p != k:
+            A[[k, p], :] = A[[p, k], :]
+            perm[k], perm[p] = perm[p], perm[k]
+
+        if A[k, k] == 0:
+            continue
+
+        # eliminate entries below the pivot
+        for i in range(k + 1, n):
+            A[i, k] /= A[k, k]
+            A[i, k + 1:] -= A[i, k] * A[k, k + 1:]
+
+        # get P, L, U matrices
+    P = np.eye(n)[perm]
+    L = np.tril(A, k=-1) + np.eye(n)
+    U = np.triu(A)
+
+    return P, L, U
---
+++ decompositions/qr.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def decompose_qr(A, method='mgs'):
+    if method == 'mgs':
+        return decompose_qr_mgs(A)
+    elif method == 'gs':
+        return decompose_qr_gs(A)
+    else: 
+        raise ValueError("Specified method not supported and/or invalid.")
+
+def decompose_qr_gs(A):
+    A = A.copy().astype(float) 
+    m, n = A.shape
+    Q = np.zeros((m, n))
+    R = np.zeros((n, n))
+
+    for j in range(n):
+        u = A[:, j]
+        for i in range(j):
+            R[i, j] = np.dot(Q[:, i], A[:, j])
+            u -= np.dot(R[i, j], Q[:, i])
+
+        R[j, j] = np.linalg.norm(u)
+        if R[j, j] == 0:
+            raise ValueError("Matrix is not full rank.")
+        
+        Q[:, j] = u / R[j, j]
+    
+    return Q, R
+
+
+def decompose_qr_mgs(A):
+    A = A.copy().astype(float) 
+    m, n = A.shape
+    Q = np.zeros((m, n))
+    R = np.zeros((n, n))
+    
+    for j in range(n):
+        u_j = A[:, j]
+        R[j, j] = np.linalg.norm(u_j)
+        if R[j, j] == 0:
+            raise ValueError("Matrix is not full rank.")
+        Q[:, j] = u_j / R[j, j]
+        for k in range(j+1, n):
+            R[j, k] = np.dot(Q[:, j], A[:, k]) 
+            A[:, k] -= R[j, k] * Q[:, j] # store u_k inside A[:, k]
+            
+    return Q, R

e1bed122f3f97ad061c28364b4012e1d9a93061a
Tommy Nguyen <haidangasian@gmail.com>
2025-07-14 21:45:19 +0700

    corrected matrix check for full col rank

--- utils/validation.py
+++ utils/validation.py
@@ -4,7 +4,7 @@ def validate_matrix_square(A):
     return A.shape[0] == A.shape[1]
 
 def validate_matrix_full_col_rank(A):
-    return np.linalg.matrix_rank(A) == A.shape[0]
+    return np.linalg.matrix_rank(A) == A.shape[1]
 
 def validate_matrix_positive_definite(A):
     return validate_matrix_square(A) and np.all(np.linalg.eigvals(A) > 0)
